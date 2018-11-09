d43cbf16b486ab95cb942d6c43f04c0b74edba90
nicola <nicola@etarom.com>
2018-11-09 09:39:27 +0100

    add helper

--- Mustache.php
+++ Mustache.php
@@ -46,7 +46,12 @@ class Mustache extends \Slim\View
     /**
      * @var string The path to the Mustache code directory WITHOUT the trailing slash
      */
-    public $parserDirectory = null;
+    public static $parserDirectory = null;
+	public static $cacheDirectory = null;
+	public static $ioDomain = null;
+	public static $cssversion = null;
+	public static $jsversion = null;
+	public static $imgversion = null;
 
     /**
      * @var array The options for the Mustache engine, see
@@ -95,6 +100,113 @@ class Mustache extends \Slim\View
 
             $parserOptions = array(
                 'loader' => new \Mustache_Loader_FilesystemLoader($this->getTemplatesDirectory()),
+                'cache' =>self:: $cacheDirectory,
+				'cache_file_mode' => 0666, // Please, configure your umask instead of doing this :)
+				'cache_lambda_templates' => true,
+				'helpers' => array(
+        			'dlang' => function($text, $mustache) {
+	        			return "{{".$mustache->render($text)."}}";
+	        		},'dpartial' => function($text, $mustache) {
+						return "{{>".$mustache->render($text)."}}";
+					},'lower' => function($text, $mustache){
+						return strtolower ($mustache->render($text));
+					},'upper'=> function($text, $mustache){
+						return strtoupper ($mustache->render($text));
+					},'cut_text'=>function($text, $mustache){
+						$max_length = 230;//default
+						$search = strtr($text, array('{' => '', '}' => ''));
+						$pieces = explode("|", $search);
+						$newt=$pieces[0];
+						$max_length=$pieces[1];
+
+						$text2=strip_tags($mustache->render('{{{'.$newt.'}}}'),'<b><strong><a>');
+						if (strlen($text2) > $max_length){
+						    $offset = ($max_length - 3) - strlen($text2);
+						    $s = substr($text2, 0, strrpos($text2, ' ', $offset)) . '...';
+						}else{
+							$s=$text2;
+						}
+						return $s;
+					},'anchor_url'=>function($text, $mustache){
+						if(!$text)
+							return "";
+						return  substr($mustache->render($text), 1);
+					},'ucw'=>function($text, $mustache){
+						return ucwords (strtolower ($mustache->render($text)));	
+					},'user_img'=>function($text, $mustache){
+						if(substr($mustache->render($text),0,4)=='http')
+						return $mustache->render($text);
+						else
+						return 	$mustache->render("/".$text);
+					},'date_it' =>function($text, $mustache){
+                        if(!$mustache->render($text))
+                            return "";
+						$date=date('d/m/Y', strtotime($mustache->render($text)));
+						return $mustache->render($date);
+					},'date_it_ext' =>function($text, $mustache){
+                        if(!$mustache->render($text))
+                            return "";
+                        setlocale(LC_TIME, "it_IT.UTF-8");
+                        $date=strftime('%A %d %B %Y',strtotime($mustache->render($text)));
+						return $mustache->render($date);
+					},'clean' => function($text, $mustache) {
+						$output = iconv("utf-8", "ascii//TRANSLIT//IGNORE", $mustache->render($text)); 
+						//lets remove utf-8 special characters except blank spaces
+						$output = preg_replace("/^'|[^A-Za-z0-9\s-]|'$/", '', $output);
+						$output = str_replace(' ', '-', $output);
+						// clean up multiple '-' characters
+						$output = preg_replace('/-{2,}/', '-', $output); 
+						// remove trailing '-' character if string not just '-'
+						if ($output != '-')
+						$output = rtrim($output, '-');
+						return strtolower (htmlspecialchars($output));
+					},'file_name'=>function($text, $mustache){
+						if(file_exists($mustache->render($text))){
+						$path_parts = pathinfo($mustache->render($text));
+						$filename=$path_parts['basename'];
+						return $filename;
+						}else{
+						return "";
+						}
+					},'file_to_route'=>function($text, $mustache){
+						if(file_exists($mustache->render($text))){
+						$path_parts = pathinfo($mustache->render($text));
+						$dirname=$path_parts['dirname'];
+						$basepath=$path_parts['basename'];
+						$filename=$path_parts['filename'];
+						$extension=$path_parts['extension'];
+						return $dirname."/".$filename."/".$extension;
+						}else{
+						return "";
+						}
+					},'tumb'=>function($text, $mustache){
+						if(file_exists($mustache->render($text))){
+						$path_parts = pathinfo($mustache->render($text));
+						$dirname=$path_parts['dirname'];
+						$basepath=$path_parts['basename'];
+						$filename=$path_parts['filename'];
+						$extension=$path_parts['extension'];
+						return $dirname."/thumbnails/".$filename.".".$extension;
+						}else{
+						return "";
+						}
+					},'bksrc'=>function($text, $mustache){
+						if(file_exists("./".$mustache->render($text))){
+						return "/".$mustache->render($text);
+						}else{
+						return "http://".Mustache::$ioDomain."/".$mustache->render($text);
+						}
+					},'cssv'=>function($text, $mustache){
+						return $mustache->render($text)."?v=".Mustache::$cssversion;
+					},'jsv'=>function($text, $mustache){
+						return $mustache->render($text)."?v=".Mustache::$jsversion;
+					},'imgv'=>function($text, $mustache){
+						return $mustache->render($text)."?v=".Mustache::$imgversion;
+					},'slugify'=>function($text, $mustache){
+						return makeSlugs($mustache->render($text), $maxlen=0);
+					}			
+	        	)
+
             );
 
             // Check if the partials directory exists, otherwise Mustache will throw a exception
@@ -106,7 +218,6 @@ class Mustache extends \Slim\View
 
             $this->parserInstance = new \Mustache_Engine($parserOptions);
         }
-
         return $this->parserInstance;
     }
 }
